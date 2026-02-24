8b530010018b7a9588f6178cec6b11c0e5286920
Himani-R <ranahimani025@gmail.com>
2026-02-24 16:38:45 +0530

    this is weather app

---
+++ src/Weather.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import './Weather.css'
+function Weather() {
+
+    const time = new Date();
+    const date = time.toLocaleTimeString();
+    const timee = time.toLocaleDateString();
+
+
+
+    const [input, setInput] = useState("")
+    const [weather, setWeather] = useState(null)
+    const key_data = "0e4872b94a53173dbd60d8796fccb526"
+
+    async function getAllData() {
+    
+          try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${key_data}&units=metric`
+        );
+
+        const jsonData = await response.json();
+
+        if (jsonData.cod !== 200) {
+            alert(jsonData.message || "City not found");
+            return;
+        }
+
+        setWeather(jsonData);
+        console.log(jsonData);
+
+    } catch (error) {
+        console.log(error);
+        alert("Something went wrong");
+    }
+
+
+
+
+
+
+
+
+    }
+
+
+    return (
+        <>
+            <div className="container">
+                <h1>Your's City Weather </h1>
+                <div className="input_fields">
+                    <input type="text" placeholder='Type Your city name :-' value={input} onChange={(e) => setInput(e.target.value)} />
+                    <button onClick={getAllData} >Submit</button>
+                </div>
+
+                {weather && (
+                    <div className='data'>
+                        <p className='icon'><img className="icon" src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`} /></p>
+                        <div className='temp'>
+                            <p>Min: {weather.main.temp_min} </p>
+                            <p> Max:  {weather.main.temp_max} </p></div>
+                        <div className='date'>  <p>Today's</p>
+                            <p >{timee}   -  {date}</p></div>
+                        <div className='speed'>
+                            <p> Speed:-  {weather.wind.speed}</p>
+                            <p> Humdity:-  {weather.main.humidity}</p>
+                        </div>
+
+
+
+
+                    </div>
+
+                )}
+            </div>
+
+        </>
+    )
+}
+
+export default Weather
+
+
+
+
+
+
+
+
+
